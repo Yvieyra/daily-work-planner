06af8a06a18908e7954d098452515fb03fcc544b
Ying Yang <vieyrayessenia8@gmail.com>
2022-11-20 02:46:42 -0700

    added more divs to complete work day schedule

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var pastTime;
+var currentTime;
+var futureTime;
+
+
 $(function () {
   var today = dayjs();
   $('#currentDay').text(today.format('dddd, MMM D, YYYY'));
@@ -17,10 +22,15 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+  //dayjs().hour() // gets current hour
+  //newDate = dayjs().hour(12) // returns new dayjs object
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+
   //
-  // TODO: Add code to display the current date in the header of the page.
+  
 });
